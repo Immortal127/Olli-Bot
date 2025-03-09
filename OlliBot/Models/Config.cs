86cd5b65c194226d65b73495f469830bd9afc145
Olliver Hughes <ohughes1207@gmail.com>
2025-03-09 06:07:09 +0000

    remove comment

--- OlliBot/Models/Config.cs
+++ OlliBot/Models/Config.cs
@@ -4,17 +4,15 @@ using System.Text.Json.Serialization;
 namespace OlliBot.Data
 {
     public class Config
-    {
-        //Rename JsonPropertyName to JsonProperty if Newtonsoft.json is prefered
-        
-        //These properties cannot be modified through commands
+    {        
+        //Properties that can't be modified through commands
         [JsonPropertyName("OwnerID")]
         public ulong OwnerID { get; init; }
         [JsonPropertyName("BotID")]
         public ulong BotID { get; init; }
 
 
-        //Properties from here onwards can be modified through commands
+        //Properties that can be modified through commands
         [JsonPropertyName("BotChannel")]
         public ulong? BotChannel { get; set; }
     }
