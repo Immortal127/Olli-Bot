d9a003c7436570e5b953dea017299a475f710726
Olliver Hughes <ohughes1207@gmail.com>
2026-08-09 08:59:43 +0100

    clean up

--- OlliBot.Bot/DependencyInjection.cs
+++ OlliBot.Bot/DependencyInjection.cs
@@ -1,7 +1,6 @@
 ﻿using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using OlliBot.Application.EmoteRanking.Scanning;
 using OlliBot.Bot.Interfaces;
 using OlliBot.Bot.Mappers;
